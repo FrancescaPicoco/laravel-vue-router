a6c0dd8dbb22e0626eac3dcb16b1d6e0dc7b305f
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-02-08 17:13:24 +0100

    axios chiamata e stampa ok

--- src/App.vue
+++ src/App.vue
@@ -17,32 +17,51 @@ export default {
 		this.getEventList();
 	},
     methods: {
-
-        getEventList() {
-
-	    let url = this.store.apiUrl + this.store.apiEndPoint;
-        console.log(url);
-		axios.get("url").then(risultato => {
-			console.log(risultato.status); //status ok 
-			console.log(risultato.data.success); //undefined
-			if(risultato.status === 200 && risultato.data.success){
-				console.log(risultato.data.payload);
-				this.store.eventList = risultato.data.payload;
-				console.log(risultato.data.payload);
-			}else{
-				console.log("something went wrong")
-			}
-		}).catch(errore => {
-			console.error(errore);
-		});
-		}
-    }
-}
+    getEventList() {
+      let url = this.store.apiUrl + this.store.apiEndPoint;
+      axios.get(url).then((risultato) => {
+          if (risultato.status === 200 && risultato.data.success) {
+            console.log(risultato.data);
+            this.store.eventList = risultato.data.results; //usare results -->vedere eventcontroller/api laravel
+          } else {
+            // controllare statusCode, presenza e veridicità di data.success
+            console.error("Something Went Wrong");
+          }
+        }).catch((errore) => {
+          console.error(errore);
+        });
+    },
+  },
+};
 </script>
 
 <template>
 	<main>
 		<AppComponent />
+		<div class="container">
+    <div>
+        <h1 class="text-center fw-bold">Eventi</h1>
+        <div class="row">
+            <div class="col-6" v-for=" event in store.eventList">                
+              <div class="card" style="width: 18rem">
+                  <!-- <img src={{ this.event.poster }} class="card-img-top" alt="event.artist" /> -->
+                   <div class="card-body">
+                      <h5 class="card-title">{{event.artist}}</h5> 
+                      <h5 class="card-title">{{event.name}}</h5>
+                      <div class="card-text">
+                       <p>{{ event.location }}</p>
+                       <p>{{ event.city }}</p>
+                       <p>{{ event.date }}</p>
+                       <p>{{ event.address }}</p>
+                       <p>{{ event.tickets }}</p>
+                      </div>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+              </div>
+            </div>
+        </div>
+    </div>
+        </div>
 
 		<button class="btn btn-primary">
 			<font-awesome-icon icon="fa-solid fa-home" class="me-1" />
--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -1,43 +1,62 @@
 <script>
-import { store } from "../store.js" //state management
-import register from "../debug" //per debuggare il componente da console
-import axios from 'axios'; //importo Axios
+import { store } from "../store.js"; //state management
+import axios from "axios"; //importo Axios
 
 export default {
-    name: "AppComponent",
-    data() {
-        return {
-            store
-        }
-    },
-    // mounted() {
-	// 	this.getEventList();
-	// },
-    // methods: {
-    //     getEventList() {
-	// 		//let url = this.store.apiUrl + this.store.apiEndPoint;
-
-	// 	axios.get("url").then(risultato => {
-	// 		if(risultato.status === 200 && risultato.data.success){
-	// 			console.log(risultato.data.payload);
-	// 			this.store.eventList = risultato.data.payload;
-	// 		}else{
-	// 			console.log("something went wrong")
-	// 		}
-	// 	}).catch(errore => {
-	// 		console.error(errore);
-	// 	});
-	// 	}
-    // }
-}
+  name: "AppComponent",
+  data() {
+    return {
+      store,
+    };
+  },
+  mounted() {
+    //this.getEventList();
+  },
+  // methods: {
+  //   getEventList() {
+  //     let url = this.store.apiUrl + this.store.apiEndPoint;
+  //     axios.get(url).then((risultato) => {
+  //         if (risultato.status === 200 && risultato.data.success) {
+  //           console.log(risultato.data.payload);
+  //           this.store.eventList = risultato.data.payload;
+  //         } else {
+  //           // controllare statusCode, presenza e veridicità di data.success
+  //           console.error("Something Went Wrong");
+  //         }
+  //       }).catch((errore) => {
+  //         console.error(errore);
+  //       });
+  //   },
+  // },
+};
 </script>
 
 <template>
+  <!-- <div class="container">
     <div>
-        <h1>Sono un componente!</h1>
+        <h1 class="text-center fw-bold">Eventi</h1>
+        <div class="row">
+            <div class="col-6" v-for=" event in this.store.eventList ">                
+              <div class="card" style="width: 18rem">
+                  <img src={{ this.event.poster }} class="card-img-top" alt="event.artist" /> -->
+                  <!-- <div class="card-body">
+                      <h5 class="card-title">{{event.artist}}</h5>
+                      <h5 class="card-title">{{event.name}}</h5>
+                      <div class="card-text">
+                       <p>{{ event.location }}</p>
+                       <p>{{ event.city }}</p>
+                       <p>{{ event.date }}</p>
+                       <p>{{ event.address }}</p>
+                       <p>{{ event.tickets }}</p>
+                      </div>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div> -->
+              <!-- </div>
+            </div>
+        </div>
     </div>
+  </div> -->
 </template>
-
 <style scoped lang="scss">
 // importo variabili
 // @use './styles/partials/variables' as *;
